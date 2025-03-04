d32087e77cf2440ae1418799a8136f8390a93029
deepakchoursiya <chourasiadeepak2002@gmail.com>
2025-03-04 11:28:26 +0530

    Deepak Add. palindrome checker

---
+++ Functions/PallindromicNumbers.js
@@ -0,0 +1,7 @@
+function isPalindrome(num) {
+    let reversed = parseInt(num.toString().split('').reverse().join(''));
+    return num === reversed;
+}
+console.log(isPalindrome(121)); 
+console.log(isPalindrome(123));
+console.log(isPalindrome(1001));
